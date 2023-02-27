2cd8a1f357f2b19732390a4420688a4c2b17fa3c
Simranjeet Singh <simran@monarch.is>
2023-02-28 00:31:31 +0530

    expire time change for admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const { userModel } = require("../models/user");
+const { AuthToken } = require("../models/authtoken");
 const jwt = require("jsonwebtoken");
 require("dotenv/config");
 const secret = process.env.secret;
@@ -12,22 +13,32 @@ exports.isAunthaticatedAdmin = async (req, res, next) => {
         message: "Header token not found.",
       });
     }
+    console.log("token",token)
+    const tokenFound = await AuthToken.findOne({ token: JSON.parse(token) });
+    console.log('tokenFound',tokenFound)
     const decode = jwt.verify(JSON.parse(token), secret);
-    if (decode.isAdmin) {
-      const userData = await userModel.findOne({ _id: decode.userId });
-      if (userData) {
-        req.user = userData;
-        //console.log('passsss')
-        next();
+    if(tokenFound){
+      if (decode.isAdmin) {
+        const userData = await userModel.findOne({ _id: decode.userId });
+        if (userData) {
+          req.user = userData;
+          //console.log('passsss')
+          next();
+        } else {
+          return res.status(401).json({
+            message: "Not Authorized",
+          });
+        }
       } else {
         return res.status(401).json({
           message: "Not Authorized",
         });
       }
-    } else {
-      return res.status(401).json({
-        message: "Not Authorized",
-      });
+    }else{
+        //await AuthToken.deleteMany({ userId: decode.userId })
+        return res.status(401).json({
+          message: "LOGOUT",
+        });
     }
   } catch (error) {
     return res.status(500).json({
