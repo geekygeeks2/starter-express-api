43665dc6e7ccc5929af14130844ef06997e2c114
himanshu.kumar <himanshu.kumar@decipherzone.com>
2024-04-30 08:53:27 +0530

    stuent login api

--- routes/public.js
+++ routes/public.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const public = require("../api/controller/public");
 
 router.post("/userlogin", public.userlogin);
+router.post("/studentLogin", public.userLoginWithPhone);
 router.post("/addUser", public.addUser);
 router.get("/getAllUser", public.getAllUser)
 router.get("/getBlogPost/:slugTitle", public.getBlogPost)
