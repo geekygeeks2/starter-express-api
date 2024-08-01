5d518978eeba665f3e03da7af769fb86ae9df3c1
Simranjeet Singh <simran@monarch.is>
2024-08-01 13:00:09 +0530

    message schema model

---
+++ models/message.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  templateType: {
+    type: String,
+    required: true,
+    default:'general'
+  },
+  recipientType: {
+    type: String,
+    required: true,
+    default:'individual'
+  },
+  messageId:{
+    type: String,
+    required: true,
+  },
+  messageData:{
+    type: JSON,
+  },
+  created: {
+    type: Date,
+  },
+  modified: {
+    type: Date,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+messageSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+messageSchema.set("toJSON", {
+  virtuals: true,
+});
+
+messageSchema.pre("save", function (next) {
+  now = new Date();
+  this.modified = now;
+  if (!this.created) {
+    this.created = now;
+  }
+  next();
+});
+
+exports.messageModel = mongoose.model("message", messageSchema);
+exports.messageSchema = messageSchema;
